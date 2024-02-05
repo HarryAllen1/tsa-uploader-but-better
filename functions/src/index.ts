923c569a2086c21f7db89bfb9aa365ea9354f71c
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2024-02-04 22:37:02 -0800

    this should work

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as logger from 'firebase-functions/logger';
-import { HttpsError, onCall } from 'firebase-functions/v2/https';
-
-export const helloWorld = onCall((request) => {
-	if (!request.auth)
-		throw new HttpsError('failed-precondition', 'The function must be called while authenticated.');
-	logger.info('Hello logs!', { structuredData: true });
-});
